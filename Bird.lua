dffcf11faa448b98bd2b70e43198e9c24fb47e64
Darius1295 <headshrinker89@gmail.com>
2020-07-11 17:09:12 +0100

    Initial commit

---
+++ Bird.lua
@@ -0,0 +1,19 @@
+Bird = Class{}
+
+function Bird:init()
+	self.image = love.graphics.newImage('bird.png')
+	self.width = self.image:getWidth()
+	self.height = self.image:getHeight()
+
+	self.x = VIRTUAL_WIDTH/2 - self.width/2
+	self.y = VIRTUAL_HEIGHT/2 - self.height/2
+end
+
+function Bird:update(dt)
+	self.dy = 10000 * dt
+	self.y = self.y + self.dy * dt
+end
+
+function Bird:render()
+    love.graphics.draw(self.image, self.x, self.y)
+end 
