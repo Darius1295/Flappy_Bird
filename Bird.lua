01b65cea43fb5b7897d35b81e5092da1492546f1
Darius1295 <headshrinker89@gmail.com>
2020-07-11 21:17:23 +0100

    Fix gravity error and add jump controls

--- Bird.lua
+++ Bird.lua
@@ -7,11 +7,17 @@ function Bird:init()
 
 	self.x = VIRTUAL_WIDTH/2 - self.width/2
 	self.y = VIRTUAL_HEIGHT/2 - self.height/2
+
+	self.dy = 0
 end
 
 function Bird:update(dt)
-	self.dy = 10000 * dt
+	self.dy = self.dy + 500 * dt
 	self.y = self.y + self.dy * dt
+end
+
+function Bird:jump()
+	self.dy = -200
 end
 
 function Bird:render()
--- main.lua
+++ main.lua
@@ -50,6 +50,10 @@ function love.update(dt)
 	groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt)
 	    % VIRTUAL_WIDTH
 
+    if love.keyboard.isDown('space') then
+    	bird:jump()
+    end
+
 	bird:update(dt)
 end
 
