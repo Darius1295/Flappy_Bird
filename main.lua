26a71086621fa16faddca36a24b7cca455735650
Darius1295 <headshrinker89@gmail.com>
2020-07-27 10:54:16 +0100

    Add top and bottom pipes, randomise distance between top and bottom pipes, create table of pipe pairs

--- main.lua
+++ main.lua
@@ -6,6 +6,8 @@ require 'Bird'
 
 require 'Pipe'
 
+require 'PipePair'
+
 WINDOW_WIDTH = 1024
 WINDOW_HEIGHT = 576
 
@@ -23,8 +25,9 @@ local GROUND_SCROLL_SPEED = 60
 local BACKGROUND_LOOPING_POINT = 413
 
 local bird = Bird()
-local pipes = {}
+local pipePairs = {}
 local spawnTimer = 0
+local lastY = -PIPE_HEIGHT + math.random(80) + 20
 
 function love.load()
 	love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -59,20 +62,27 @@ function love.update(dt)
     end
 
 	spawnTimer = spawnTimer + dt
-    if spawnTimer > 2 then
-    	table.insert(pipes, Pipe())
+    if spawnTimer > 2 then    	
+    	local y = math.max(-PIPE_HEIGHT + 10, 
+            math.min(lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
+        lastY = y
+
+    	table.insert(pipePairs, PipePair(y))
     	spawnTimer = 0
     end
 
     bird:update(dt)
 
-    for k, pipe in pairs(pipes) do
+    for k, pipe in pairs(pipePairs) do
     	pipe:update(dt)
-
-    	if pipe.x < -pipe.width then
-    		table.remove(pipes, k)
-    	end
     end
+
+    for k, pair in pairs(pipePairs) do
+        if pair.remove then
+            table.remove(pipePairs, k)
+        end
+    end
+
 end
 
 function love.draw()
@@ -80,8 +90,8 @@ function love.draw()
 
     love.graphics.draw(background, -backgroundScroll, 0)
 
-    for k, pipe in pairs(pipes) do
-    	pipe:render()
+    for k, pair in pairs(pipePairs) do
+    	pair:render()
     end
 
     love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
